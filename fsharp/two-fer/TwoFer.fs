e473e559a608046fe7abfb904e0c70182a3a221e
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-13 13:26:28 +0530

    completed fsharp-exercise-2

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,7 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string = 
+   match input with 
+    | None -> "One for you, one for me."
+    | Some (name) -> sprintf "One for %s, one for me." name
+
