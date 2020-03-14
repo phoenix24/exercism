a9c46b1e3fec8b4dfedb16727af67a1ae770b1de
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-14 15:24:31 +0530

    completed python exercise-9

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,20 @@
+def score(word):
+    points = {
+        "D": 2,
+        "G": 2,
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3,
+        "F": 4,
+        "H": 4,
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10,
+    }
+    return sum([points.get(w, 1) for w in word.upper()])
