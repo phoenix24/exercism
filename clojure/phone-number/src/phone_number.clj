3cd748f6f5abb43efa3ef3d4ce0d57da94606f32
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-14 22:47:47 +0530

    refactored clojure-exercise-5

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,5 +1,7 @@
 (ns phone-number)
 
+(def invalid-number "0000000000")
+
 (defn clean [number]
   (apply str
     (filter (fn [x] (Character/isDigit x)) number)))
@@ -9,7 +11,7 @@
      (cond
        (= 10 ll-number) number 
        (and (= 11 ll-number) (= "1" (subs number 0 1))) (subs number 1)
-       :else "0000000000")))
+       :else invalid-number)))
 
 (defn number [num-string]
   (validate 
@@ -19,8 +21,8 @@
   (subs (number num-string) 0 3))
 
 (defn pretty-print [num-string] 
-   (let [cl-number (number num-string)]
-      (format "(%s) %s-%s"
-         (subs cl-number 0 3)
-      	 (subs cl-number 3 6)
-	 (subs cl-number 6 10))))
+   (let [cl-number (number num-string)
+   	 area-code (subs cl-number 0 3)
+	 prefix    (subs cl-number 3 6)
+	 suffix    (subs cl-number 6 10)]
+      (format "(%s) %s-%s" area-code prefix suffix)))
