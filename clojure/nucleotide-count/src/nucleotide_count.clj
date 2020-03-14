162ca72a23369b994e0f68021d4b0d050b961e2d
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-15 03:14:15 +0530

    refactored clojure-exercise-8, from feedback, using pre-conditions + frequencies.

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,11 +1,8 @@
 (ns nucleotide-count)
 
-(def nucleotides {\A 1 \T 1 \C 1 \G 1})
-
 (defn count-of-nucleotide-in-strand [nu strand]
-  (if (contains? nucleotides nu)
-    (count (filter (fn [x] (= x nu)) strand))
-    (throw (Exception. "throwing exception invalid nucleotide"))))
+  {:pre [(contains? #{\A \T \C \G} nu)]}
+  (count (filter (fn [x] (= x nu)) strand)))
 
 (defn nucleotide-counts [strand]
    (merge {\A 0 \T 0 \C 0 \G 0}
