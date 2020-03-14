4dceb193777f2dab7ae47ed2531eb5c4e0520bdc
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-15 03:17:37 +0530

    refactored clojure-exercise-8, from feedback

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,9 @@
 (ns nucleotide-count)
 
-(defn count-of-nucleotide-in-strand [nu strand]
-  {:pre [(contains? #{\A \T \C \G} nu)]}
-  (count (filter (fn [x] (= x nu)) strand)))
-
 (defn nucleotide-counts [strand]
    (merge {\A 0 \T 0 \C 0 \G 0}
          (frequencies strand)))
 
+(defn count-of-nucleotide-in-strand [nu strand]
+  {:pre [(contains? #{\A \T \C \G} nu)]}
+  ((nucleotide-counts strand) nu))
