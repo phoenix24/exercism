aa427f42bd02480ac6d60d22b8537747fe5216a6
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-17 02:43:06 +0530

    refactored clojure-exercise-10, from feedback

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,21 +1,7 @@
 (ns leap)
 
-(defn divv [x] 
-  (fn [y] 
-    (= 0 (mod y x))))
-
-(def div4
-  (divv 4))
-
-(def div100 
-  (divv 100))
-
-(def div400
-  (divv 400))
-
 (defn leap-year? [year]
-  (or
-     (div400 year)
-     (and
-        (div4 year)
-        (not (div100 year)))))
+  (letfn [(div? [n] (zero? (mod year n)))]
+     (or (div? 400)
+         (and (div? 4)
+	      (not (div? 100))))))
