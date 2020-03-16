d05c738f58c9b5f289e85fa4325a7bdce46c5d7e
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-16 11:57:53 +0530

    refactored python-exercise-15

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,12 +1,12 @@
 class Clock:
     def __init__(self, hour, minute):
-        self.ms = 60 * hour + minute
-        self.hour, self.minute = self.__time(60 * hour + minute)
+        self.hour, self.minute = self.__time(hour, minute)
 
-    def __time(self, minutes):
-        hour = minutes // 60
-        minutes = (minutes - hour * 60) % 60
-        return (hour % 24, minutes)
+    def __time(self, hour, minute):
+        ms = 60 * hour + minute
+        hours = ms // 60
+        minutes = (ms - hours * 60) % 60
+        return (hours % 24, minutes)
 
     def __repr__(self):
         return "%02d:%02d" % (self.hour, self.minute)
@@ -16,10 +16,10 @@ class Clock:
             and self.minute == other.minute
 
     def __add__(self, minutes):
-        hour, minutes = self.__time(self.ms + minutes)
+        hour, minutes = self.__time(self.hour, self.minute + minutes)
         return Clock(hour, minutes)
 
     def __sub__(self, minutes):
-        hour, minutes = self.__time(self.ms - minutes)
+        hour, minutes = self.__time(self.hour, self.minute - minutes)
         return Clock(hour, minutes)
 
