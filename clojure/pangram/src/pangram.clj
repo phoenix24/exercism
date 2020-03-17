73bdb78d5d9ebdb1a3738601dae645be66b5b113
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-17 18:56:38 +0530

    refactored clojure-exercise-11, from feedback, learned about every? and -> thread form.

--- clojure/pangram/src/pangram.clj
+++ clojure/pangram/src/pangram.clj
@@ -3,15 +3,7 @@
 
 (def alphas (set "abcdefghijklmnopqrstuvwxyz"))
 
-(defn lower [x]
-  (clojure.string/lower-case x))
-
-(defn isalpha? [x]
-  (Character/isLetter x))
-
-(defn clean [msg]
-  (set 
-    (filter isalpha? (lower msg))))
-
 (defn pangram? [msg]
-  (if (= alphas (clean msg)) true false))
+   (every? 
+      (set (clojure.string/lower-case msg)) 
+      alphas))
