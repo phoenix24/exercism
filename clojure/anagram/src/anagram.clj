f24f32a2481cc4abf8a854c42c589ed03b679aeb
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-18 13:43:47 +0530

    completed clojure-exercise-16, fixed indentation.

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -6,7 +6,9 @@
 	lw2 (str/lower-case word2)
 	fw1 (frequencies lw1)
 	fw2 (frequencies lw2)]
-	(and (= fw1 fw2) (not= lw1 lw2))))
+	(and 
+	  (= fw1 fw2) 
+	  (not= lw1 lw2))))
 
 (defn anagrams-for [word prospect-list]
    (or (filter #(isanagram? word %) prospect-list) []))
