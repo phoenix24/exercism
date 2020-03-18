e12f4e8c9d598f216a468a7c03ff387b5205357d
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-18 13:39:57 +0530

    completed clojure-exercise-16.

---
+++ clojure/anagram/src/anagram.clj
@@ -0,0 +1,12 @@
+(ns anagram
+  (:require [clojure.string :as str]))
+
+(defn isanagram? [word1 word2] 
+  (let [lw1 (str/lower-case word1)
+	lw2 (str/lower-case word2)
+	fw1 (frequencies lw1)
+	fw2 (frequencies lw2)]
+	(and (= fw1 fw2) (not= lw1 lw2))))
+
+(defn anagrams-for [word prospect-list]
+   (or (filter #(isanagram? word %) prospect-list) []))
