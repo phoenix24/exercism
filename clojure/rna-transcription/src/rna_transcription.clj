3b7a32237e1d01b2ab78f773245f2f0a816757a2
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-18 13:04:36 +0530

    completed clojure-exercise-15, another implmentation.

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,12 +1,11 @@
 (ns rna-transcription)
 
 (defn trans [x]
-   (cond 
-      (= x \G) "C"
-      (= x \C) "G"
-      (= x \T) "A"
-      (= x \A) "U"
-      :else (throw (AssertionError. "invalid element"))))
+   (let [trans {\G \C \C \G \T \A \A \U}
+         elemt (trans x)]
+      (cond 
+         (some? elemt) elemt
+         :else (throw (AssertionError. "invalid element")))))
 
 (defn to-rna [dna]
    (apply str (map trans (seq dna))))
