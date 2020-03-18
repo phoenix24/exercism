6d8e2286380bb90ba8183138cebc5657e624b838
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-18 11:54:29 +0530

    completed clojure-exercise-14, updated based on feedback.

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,4 +1,10 @@
 (ns reverse-string)
 
-(defn reverse-string [s]
+(defn reverse-string-1 [s]
    (apply str (reverse s)))
+
+(defn reverse-string-2 [s]
+   (apply str (into () s)))
+
+(defn reverse-string [s]
+   (reduce #(str %2 %1) "" s))
