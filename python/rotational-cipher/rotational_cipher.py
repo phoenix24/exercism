9ad9d02431bc82875b2cc85ccdcd6720aa140d4e
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-20 13:04:58 +0530

    completed python exercise-29., updated from community feedback

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,11 +1,6 @@
+
 def rotate(text, key):
-    lalpha = "abcdefghijklmnopqrstuvwxyz"
-    ualpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    def next(t):
-        alpha = lalpha if t in lalpha else ualpha
-        offset = ord('a') if t in lalpha else ord('A')
-        return alpha[ (ord(t) - offset + key) % 26 ]
-
-    alpha = lambda x: x if not x.isalpha() else next(x)
-    return "".join([alpha(t) for i, t in enumerate(text)])
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    nchars = chars[key:] + chars[:key]
+    ttable = str.maketrans(chars + chars.upper(), nchars + nchars.upper())
+    return text.translate(ttable)
