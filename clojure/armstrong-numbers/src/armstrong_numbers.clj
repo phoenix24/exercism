8ee0f10e3c30aed386d6188d50b0ebf22fc5e0ae
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-21 15:16:06 +0530

    completed clojure-exercise-18, using thread macros; community feedback

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,18 +1,9 @@
 (ns armstrong-numbers)
 
-(defn exp [e x] 
-   (reduce * (repeat x e)))
-
-(defn carm [lnum]
-   (let [pow (count lnum)
-         exps (map #(exp % pow) lnum)]
-      (reduce + exps)))
-
-(defn lint [snum]
-   (map #(- (int %) 48) (seq snum)))
-
 (defn armstrong? [num] 
-   (let [snum (str num)
-         lnum (lint snum)
-	 anum (carm lnum)]
-      	 (= anum num)))
+   (let [digits (->> num str (map #(Character/digit % 10)))
+         power (count digits)]
+      (->> digits
+           (map #(Math/pow % power))
+	   (apply +)
+	   (== num))))
