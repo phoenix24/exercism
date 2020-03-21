7249cb5e7362d1e372edd188587ec9d1f7236b20
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-21 16:20:53 +0530

    completed clojure-exercise-19, using re-seq community feedback

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,16 +1,6 @@
 (ns word-count
   (:require [clojure.string :as str]))
 
-(defn isalpha? [x]
-   (or 
-     (Character/isDigit x)
-     (Character/isSpace x)
-     (Character/isLetter x)))
-
 (defn word-count [s] 
-   (let [string (->> s
-                  (filter isalpha?)
-		  (apply str)
-		  (str/trim)
-		  (str/lower-case))]
-      (frequencies (str/split string #"\s+"))))
+   (frequencies (re-seq #"\w+" (str/lower-case s))))
+
