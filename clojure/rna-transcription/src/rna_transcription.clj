ebd75d224931aa6eb3ff7dfbcb31493ecfb89f9e
Chaitanya Sharma <chaitanya@3bandar.org>
2020-03-27 02:19:08 +0530

    refactored from mentor feedback!

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,11 +1,12 @@
 (ns rna-transcription)
 
-(defn trans [x]
-   (let [trans {\G \C \C \G \T \A \A \U}
-         elemt (trans x)]
-      (cond 
-         (some? elemt) elemt
-         :else (throw (AssertionError. "invalid element")))))
-
+(defn- trans [x]
+   (case x 
+       \G \C
+       \C \G
+       \T \A 
+       \A \U
+       (throw (AssertionError. "Wrong input."))))
+     
 (defn to-rna [dna]
-   (apply str (map trans (seq dna))))
+   (apply str (map trans dna)))
